19a162c421d8c3961eff1e47cb96b64da9fb65b0
Henrik Frisk <frisk.h@gmail.com>
2025-09-11 13:31:14 +0200

    Updated the script to work in the new position.

--- stuff/import_data.sh
+++ stuff/import_data.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
-
-pulset=Impulse
-sustain=Iteration
-base="/Volumes/Freedom/Dropbox/Documents/kmh/forskning/applications/KK/KKS 2022 IRESAP/classification/training_data/"
-imp="$base""$pulset"
-iter="$base""$sustain"
+dataset="../../dataset/SCHAEFFER/"
+keyA=Impulse
+keyB=Iteration
+base="../classification/training_data/"
+dirA="$base""$keyA"
+dirB="$base""$keyB"
 
 echo "Clearing directories...\n\n"
-rm -rf $iter/*.wav
-rm -rf $imp/*.wav
+rm -rf $dirB/*.wav
+rm -rf $dirA/*.wav
 
 echo "Copying files from dataset...\n\n"
-for d in */; do
+for d in "$dataset"*/; do
     for j in "$d"*.json; do
 	if test -f "$j"
 	then
-	    if [[ "$(cat "$j" | jq '.object.labels.sustain')" == "\""$sustain"\"" ]]; then
-		if [ ! -d "$iter" ]; then
-		    mkdir "$iter"
+	    if [[ "$(cat "$j" | jq '.object.labels.sustain')" == "\""$keyB"\"" ]]; then
+		if [ ! -d "$dirB" ]; then
+		    mkdir "$dirB"
 		fi
-		cp "$d""`cat "$j" | jq -r '.object.filename'`" "$iter"
+		cp "$d""`cat "$j" | jq -r '.object.filename'`" "$dirB"
 	    fi
-	    if [[ "$(cat "$j" | jq '.object.labels."pulse-typology"')" == "\""$pulset"\"" ]]; then
-		if [ ! -d "$imp" ]; then
-		    mkdir "$imp"
+	    if [[ "$(cat "$j" | jq '.object.labels."pulse-typology"')" == "\""$keyA"\"" ]]; then
+		if [ ! -d "$dirA" ]; then
+		    mkdir "$dirA"
 		fi
-		cp "$d""`cat "$j" | jq -r '.object.filename'`" "$imp"
+		cp "$d""`cat "$j" | jq -r '.object.filename'`" "$dirA"
 	    fi
 	fi
     done
