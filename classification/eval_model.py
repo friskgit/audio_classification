7d902c58e1a8c5f0814b55709d9c7fcac308e677
Henrik Frisk <frisk.h@gmail.com>
2025-09-21 16:45:12 +0200

    Started rewriting output of eval_model.py

--- classification/eval_model.py
+++ classification/eval_model.py
@@ -2,17 +2,17 @@ test_accuracy=model.evaluate(X_test,y_test,verbose=0)
 print(test_accuracy[1])
 
 import glob
+
 # Load the saved model
-# dir = os.getcwd()
-dir = '/Volumes/Freedom/Dropbox/Documents/kmh/forskning/applications/KK/KKS 2022 IRESAP/audio_classification/classification/'
-model = load_model(dir + 'audio_classification.keras')
+dir = os.getcwd()
+model = load_model(os.path.join(dir, 'audio_classification.keras'))
 
 # Define the target shape for input spectrograms
 target_shape = (256, 128)
 
 # Define your class labels
 classes = ['HarmSus', 'HarmImp', 'HarmIter', 'NoiseSus', 'NoiseImp', 'NoiseIter', 'CompositeSus', 'CompositeImp', 'CompositeIter']
-
+tartyp = ['N0, N1, N2, X0, X1, X2, Y0, Y1, Y2']
 # Function to preprocess and classify an audio file
 def test_audio(file_path, model):
     # Load and preprocess the audio file
@@ -33,19 +33,23 @@ def test_audio(file_path, model):
     return class_probabilities, predicted_class_index
 
 # Test an audio file
-test_audio_dir = '/Volumes/Freedom/Dropbox/Documents/kmh/forskning/applications/KK/KKS 2022 IRESAP/audio_classification/classification/training_data'
+test_audio_dir = os.path.join(dir, 'training_data/testing/')
+
+print(test_audio_dir)
 
 extension = "*.wav"
 for test_audio_file in glob.glob(os.path.join(test_audio_dir, extension)):
     class_probabilities, predicted_class_index = test_audio(test_audio_file, model)
-
+    print(f'{os.path.basename(test_audio_file)}, , ,')
+    print(f'Class, Probability, Accuracy, Classified as, Analyzed as')
     # Display results for all classes
     for i, class_label in enumerate(classes):
         probability = class_probabilities[i]
-        print(f'Class: {class_label}, Probability: {probability:.4f}')
+        print(f'{class_label}, {probability:.4f}')
 
     # Calculate and display the predicted class and accuracy
     predicted_class = classes[predicted_class_index]
+#    if('{os.path.basename(test_audio_file)}' )
     accuracy = class_probabilities[predicted_class_index]
-    print(f'The audio {os.path.basename(test_audio_file)} is classified as: {predicted_class}')
-    print(f'Accuracy: {accuracy:.4f}')
+    print(f', , {accuracy:.4f}, {predicted_class}')
+
