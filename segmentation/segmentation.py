de73b25fd2bbfeb4160def79ec3c39b2ebd7a2c6
Henrik Frisk <frisk.h@gmail.com>
2026-01-19 12:00:53 +0100

    Updated segmentation scripts

--- segmentation/segmentation.py
+++ segmentation/segmentation.py
@@ -4,14 +4,14 @@ from maad.util import plot_spectrogram
 import matplotlib.pyplot as plt
 import soundfile as sf
 
-s, fs = sound.load('/Volumes/Freedom/Dropbox/Documents/kmh/forskning/applications/KK/KKS 2022 IRESAP/audio_classification/segmentation/audio/dataton2.wav')
+s, fs = sound.load('/Volumes/Freedom/Dropbox/Documents/kmh/forskning/applications/KK/KKS 2022 IRESAP/audio_classification/segmentation/audio/blaa.wav')
 Sxx, tn, fn, ext = sound.spectrogram(s, fs, nperseg=1024, noverlap=512)
-plot_spectrogram(Sxx, extent=ext, db_range=60, gain=20, colorbar=False, figsize=(2.5,10))
+# plot_spectrogram(Sxx, extent=ext, db_range=60, gain=20, colorbar=False, figsize=(2.5,10))
 
-regions = find_rois_cwt(s, fs, flims=(80,10000), tlen=3, th=0, display=False, figsize=(10,6))
+regions = find_rois_cwt(s, fs, flims=(500,10000), tlen=3, th=0, display=False, figsize=(10,6))
 
 plt.show()
-print(df_trill)
+# print(df_trill)
 print(regions.head())
 
 for i, row in regions.iterrows():
--- segmentation/segmentation_print.py
+++ segmentation/segmentation_print.py
@@ -5,11 +5,11 @@ import matplotlib.pyplot as plt
 import soundfile as sf
 
 split = []
-s, fs = sound.load('./audio/dataton2.wav')
+s, fs = sound.load('./audio/sax.wav')
 Sxx, tn, fn, ext = sound.spectrogram(s, fs, nperseg=1024, noverlap=512)
-plot_spectrogram(Sxx, extent=ext, db_range=60, gain=20, colorbar=False, figsize=(2.5,10))
+# plot_spectrogram(Sxx, extent=ext, db_range=60, gain=20, colorbar=False, figsize=(2.5,10))
 
-regions = find_rois_cwt(s, fs, flims=(7000,10000), tlen=4, th=0, display=True, figsize=(10,6))
+regions = find_rois_cwt(s, fs, flims=(500,10000), tlen=3, th=0.025, display=True, figsize=(10,6))
 
 plt.show()
 print(regions.head)
