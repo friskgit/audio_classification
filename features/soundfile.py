f5d62e96acaa452ed5080067d7439f11f17178b5
Henrik Frisk <frisk.h@gmail.com>
2026-04-06 12:35:19 +0200

    Working version analysing and reading data

--- features/soundfile.py
+++ features/soundfile.py
@@ -23,11 +23,50 @@ def read_json(directory):
   files = os.listdir(directory)
   soundfiles = [] # instances of the SoundFile class
   for f in files:
-    if f.endswith('.json'):
-      print(f)
+    # first create SoundFile instances for each 'feat-0.json
+    if f.endswith('feat-0.json'):
       full_path = os.path.join(directory, f)
       with open(full_path) as json_file:
         data = json.load(json_file)
-        print(data['fname'])
-        
+        audiof = data['fname']
+        directory, fname = os.path.split(audiof)
+        sf = SoundFile(directory, audiof)
+        update_sound_file(sf, data)
+        soundfiles.append(sf)
+        print(f'added {fname}')
+  for f in files:
+    # add segments to existing SoundFile instances
+    if f.endswith('.json'):
+      full_path = os.path.join(directory, f)
+      with open(full_path) as json_file:
+        data = json.load(json_file)
+        audiof = data['fname']
+        directory, fname = os.path.split(audiof)
+        if is_segmented(f) == 1:
+          for sfi in soundfiles:
+            if sfi.name == audiof:
+              update_sound_file(sfi, data)
+  return soundfiles
+
+def update_sound_file(sfi, data):
+  sfi.analysis.append(data)
+  sfi.start.append(data['start_time'])
+  sfi.end.append(data['end_time'])
+  return sfi
+
+def is_segmented(fname):
+  """Returns -1 if fname is not a segment, 0 if the index is > 0 and 1 if the index is equal to 0"""
+  import re
+  tag = fname[-10:]
+  number = re.findall(r'\d+', tag)
+  if len(number) > 0:
+    if int(number[0]) == 0:
+      return 0
+    elif int(number[0]) > 0:
+      return 1
+  else:
+    return 0
+  
+  
+
 #      soundfiles.append(SoundFile(directory))
